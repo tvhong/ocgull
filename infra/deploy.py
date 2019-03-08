946e3963a0af5a5132a7a7950235de21a751e53d
Vy Hong <vhong@zenefits.com>
2019-03-07 18:28:50 -0800

    Deploy package to aws lambda.

--- infra/deploy.py
+++ infra/deploy.py
@@ -6,16 +6,29 @@ CURRENT_DIR = path.dirname(path.realpath(__file__))
 PROJECT_ROOT = path.normpath(path.join(CURRENT_DIR, '../'))
 BUILD_DIR = path.join(PROJECT_ROOT, 'build/')
 SRC_DIR = path.join(PROJECT_ROOT, 'src/')
+PACKAGE_DIR = path.join(BUILD_DIR, 'package/')
+
+def run_command(params, **kwargs):
+    print("[cmd] {}".format(' '.join(params)))
+    if kwargs:
+        print("[cmd_kwargs] {}".format(kwargs))
+
+    subprocess.run(params, **kwargs)
 
 def build():
-    print("Building zip...")
+    print("Downloading packages...")
+    run_command(['pip', 'install', '-r', 'requirements.txt', '--target', PACKAGE_DIR])
+
     function_zip_file = path.join(BUILD_DIR, 'function.zip')
-    ocgull_file = path.join(SRC_DIR, 'ocgull.py')
-    subprocess.run(['zip', '-j', '-r', function_zip_file, ocgull_file])
+    print("Adding package to zip...")
+    run_command(['zip', '-r', function_zip_file, '.'], cwd=PACKAGE_DIR)
+
+    print("Adding src to zip...")
+    run_command(['zip', '-g', '-r', function_zip_file, '.', '-i', '*.py'], cwd=SRC_DIR)
 
 def deploy():
     print("Deploying to AWS lambda...")
-    subprocess.run(['aws', 'lambda', 'update-function-code',
+    run_command(['aws', 'lambda', 'update-function-code',
             '--function-name', 'ocgull',
             '--zip-file', 'fileb://build/function.zip'])
 
