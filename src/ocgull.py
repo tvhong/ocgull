ca5fb72867b2c869478f70ee64131849afb9ac1d
Vy Hong <vhong@zenefits.com>
2019-03-09 16:07:39 -0800

    Add Sheet type

--- src/ocgull.py
+++ src/ocgull.py
@@ -1,9 +1,11 @@
 import json
 import os
 import sys
+from collections import namedtuple
 
 from googleapiclient import discovery
 
+Sheet = namedtuple('Sheet', ['id', 'title'])
 
 class OcGull():
     SPREADSHEET_ID = '1qZCXaYM_gH8vft3InZlhEXixHOfZLtipm95FvhQ_Gqo'
@@ -15,28 +17,27 @@ class OcGull():
         """and create notification
         when there are interesting changes."""
 
-        protected_sheet_ids = self.fetch_protected_sheet_ids()
-        return protected_sheet_ids
+        return self.fetch_protected_sheet_ids()
 
     def fetch_protected_sheet_ids(self):
         """Fetch latest data from the OC signup spreadsheet."""
 
-        # Read from a stored file that contains the protected_range_sheets ids.
-        # Get current protected_range_sheets
-        # if set(protected_range_sheets) - set(old_protected_range_sheets)
-        #  write a new protected_range_sheets file
+        # Read from a stored file that contains the protected_sheets ids.
+        # Get current protected_sheets
+        # unlocked_sheets = (set(prev_protected_sheets) - set(protected_sheets)).intersect()
+        # if set(protected_sheets) - set(prev_protected_sheets)
+        #  write a new protected_sheets file
         #  create a notification
         # exit
         service = self._get_spreadsheet_service()
         spreadsheet = service.spreadsheets().get(spreadsheetId=self.SPREADSHEET_ID).execute()
-        sheets = spreadsheet.get('sheets', [])
-        protected_sheet_ids = [
-            sheet['properties']['sheetId']
-            for sheet in sheets
+        protected_sheets = [
+            Sheet(sheet['properties']['sheetId'], sheet['properties']['title'])
+            for sheet in spreadsheet.get('sheets', [])
             if sheet.get('protectedRanges')
         ]
 
-        return protected_sheet_ids
+        return protected_sheets
 
         # sheet['protectedRanges']
         # [{'protectedRangeId': 639046925, 'range': {'sheetId': 1333024752}}]
@@ -49,10 +50,9 @@ class OcGull():
 def handleLambdaEvent(event, context):
     api_key = os.environ.get('GCP_API_KEY')
     gull = OcGull(api_key)
-    protected_sheet_ids = gull.pull()
     return {
         'statusCode': 200,
-        'body': json.dumps(protected_sheet_ids)
+        'body': json.dumps(gull.pull())
     }
 
 if __name__ == '__main__':
