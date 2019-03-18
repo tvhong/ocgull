72247969d312577744debaf97df7b6fae24d734c
Vy Hong <vhong@zenefits.com>
2019-03-17 20:29:26 -0700

    Add FixtureManager.load_sheet

--- src/ocgull/fixture/fixture_manager.py
+++ src/ocgull/fixture/fixture_manager.py
@@ -1,22 +1,49 @@
 import json
 from os import path
 
+from constants import ProtectionStatus
 from fixture.constants import Fixture
 
 FIXTURE_DIR = path.dirname(path.realpath(__file__))
 
 
 class FixtureManager():
+    PROTECTION_TO_TITLE = {
+        Fixture.BEFORE: {
+            ProtectionStatus.UNPROTECTED: 'OC1 Damages',
+            ProtectionStatus.PROTECTED: 'MAR 17 - MAR 23',
+            ProtectionStatus.UNLOCKED: 'MAR 3 - MAR 9',
+        },
+        Fixture.AFTER: {
+            ProtectionStatus.UNPROTECTED: 'OC1 Damages',
+            ProtectionStatus.PROTECTED: 'MAR 24 - MAR 30',
+            ProtectionStatus.UNLOCKED: 'MAR 17 - MAR 23',
+        }
+    }
+
     @classmethod
     def load_spreadsheet(cls, fixture=Fixture.AFTER):
         """
         Load a spreadsheet fixture into memory.
 
         :param fixture: The desired fixture.
-        :return: dict string represents the spreadsheet.
+        :return: dict represents the spreadsheet.
         """
         file_path = path.join(FIXTURE_DIR, fixture)
         with open(file_path) as f:
             json_str = f.read()
 
         return json.loads(json_str)
+
+    @classmethod
+    def load_sheet(cls, protection, fixture=Fixture.AFTER):
+        """
+        Load a sheet fixture into memory.
+
+        :param protection: The desired protection level of the sheet.
+        :param fixture: The desired fixture.
+        :return: dict represents the sheet.
+        """
+        gspreadsheet = cls.load_spreadsheet(fixture)
+        return next(s for s in gspreadsheet['sheets']
+                if s['properties']['title'] == cls.PROTECTION_TO_TITLE[fixture][protection])
--- src/ocgull/spreadsheet/tests/test_sheet.py
+++ src/ocgull/spreadsheet/tests/test_sheet.py
@@ -3,18 +3,17 @@ from unittest import TestCase
 
 from constants import ProtectionStatus
 from fixture.constants import Fixture
+from fixture.fixture_manager import FixtureManager
+from spreadsheet.sheet import Sheet
 from utils import load_fixture_spreadsheet
 
 
 class TestSheet(TestCase):
     def setUp(self):
         self.spreadsheet = load_fixture_spreadsheet(Fixture.AFTER)
-        self.unprotected_sheet = [s for s in self.spreadsheet.sheets
-                if s.title == 'OC1 Damages'][0]
-        self.protected_sheet = [s for s in self.spreadsheet.sheets
-                if s.title == 'MAR 24 - MAR 30'][0]
-        self.unlocked_sheet = [s for s in self.spreadsheet.sheets
-                if s.title == 'MAR 17 - MAR 23'][0]
+        self.unprotected_sheet = Sheet(FixtureManager.load_sheet(ProtectionStatus.UNPROTECTED))
+        self.protected_sheet = Sheet(FixtureManager.load_sheet(ProtectionStatus.PROTECTED))
+        self.unlocked_sheet = Sheet(FixtureManager.load_sheet(ProtectionStatus.UNLOCKED))
 
     def test_protectedStatus_unprotectedSheet_unprotectedStatus(self):
         self.assertEqual(ProtectionStatus.UNPROTECTED,
