594c1593801090fccebc294f535148ba890d513b
Vy Hong <vhong@zenefits.com>
2019-03-23 11:27:35 -0700

    Add logging for OcGull factory

--- src/ocgull/ocgull_factory.py
+++ src/ocgull/ocgull_factory.py
@@ -1,3 +1,4 @@
+import logging
 import os
 
 from ocgull.notifier import EmailNotifier, PrintNotifier
@@ -5,6 +6,7 @@ from ocgull.ocgull import OcGull
 from ocgull.spreadsheet.repo import (PreviousSpreadsheetRepo, RepoConfig,
                                      SpreadsheetRepo)
 
+logger = logging.getLogger(__name__)
 
 class OcGullFactory():
     @classmethod
@@ -16,6 +18,8 @@ class OcGullFactory():
         :param notify_via_email: Whether the created instance should send email
                 notifications.
         """
+        logger.info("Creating OcGull with args: {}".format((datasource, notify_via_email)))
+
         repoconfig = RepoConfig(datasource)
         gcp_api_key = os.environ.get('GCP_API_KEY')
         notifier = EmailNotifier() if notify_via_email else PrintNotifier()
