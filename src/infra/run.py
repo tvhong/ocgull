a6ff257f8dace490e587423b5b33443de614236b
Vy Hong <vhong@zenefits.com>
2019-03-23 09:59:49 -0700

    Extract runner scripts to infra/run.py

---
+++ src/infra/run.py
@@ -0,0 +1,38 @@
+import json
+import logging
+import os
+import sys
+
+from ocgull.notifier import EmailNotifier, PrintNotifier
+from ocgull.ocgull_runner import OcGull
+from ocgull.spreadsheet.repo import (PreviousSpreadsheetRepo, RepoConfig,
+                                     SpreadsheetRepo)
+from ocgull.spreadsheet.repo.constants import Environment
+
+logger = logging.getLogger(__name__)
+
+
+def handleLambdaEvent(event, context):
+    logging.basicConfig(level=logging.INFO)
+
+    repoconfig = RepoConfig(Environment.PROD)
+    api_key = os.environ.get('GCP_API_KEY')
+    print("Blah")
+    logger.warn("POOP!")
+    logger.error("POOP! ERror")
+    gull = OcGull(SpreadsheetRepo(repoconfig, api_key),
+            PreviousSpreadsheetRepo(repoconfig), PrintNotifier())
+    return {
+        'statusCode': 200,
+        'body': json.dumps([(sheet.id, sheet.title, sheet.protection) for sheet in gull.pull()])
+    }
+
+
+if __name__ == '__main__':
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+    repoconfig = RepoConfig(Environment.DEV)
+    api_key = sys.argv[1]
+    gull = OcGull(SpreadsheetRepo(repoconfig, api_key), PreviousSpreadsheetRepo(repoconfig),
+            EmailNotifier())
+    print(gull.pull())
--- src/ocgull/ocgull_runner.py
+++ src/ocgull/ocgull_runner.py
@@ -1,13 +1,6 @@
-import json
 import logging
-import os
-import sys
 
 from ocgull.constants import ProtectionStatus
-from ocgull.notifier import EmailNotifier, PrintNotifier
-from ocgull.spreadsheet.repo import (PreviousSpreadsheetRepo, RepoConfig,
-                                    SpreadsheetRepo)
-from ocgull.spreadsheet.repo.constants import Environment
 
 logger = logging.getLogger(__name__)
 
@@ -43,29 +36,3 @@ class OcGull():
         result = curr_unlocked_sheets - prev_unlocked_sheets
         logger.info("Recently unlocked sheets", extra={"unlocked_sheets": result})
         return list(result)
-
-
-def handleLambdaEvent(event, context):
-    logging.basicConfig(level=logging.INFO)
-
-    repoconfig = RepoConfig(Environment.PROD)
-    api_key = os.environ.get('GCP_API_KEY')
-    print("Blah")
-    logger.warn("POOP!")
-    logger.error("POOP! ERror")
-    gull = OcGull(SpreadsheetRepo(repoconfig, api_key),
-            PreviousSpreadsheetRepo(repoconfig), PrintNotifier())
-    return {
-        'statusCode': 200,
-        'body': json.dumps([(sheet.id, sheet.title, sheet.protection) for sheet in gull.pull()])
-    }
-
-
-if __name__ == '__main__':
-    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
-
-    repoconfig = RepoConfig(Environment.DEV)
-    api_key = sys.argv[1]
-    gull = OcGull(SpreadsheetRepo(repoconfig, api_key), PreviousSpreadsheetRepo(repoconfig),
-            EmailNotifier())
-    print(gull.pull())
